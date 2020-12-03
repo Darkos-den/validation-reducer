5d20c292fcd52baefd306f9bd2005198fb2f6273
Darkos <loboda.deni@gmail.com>
2020-12-03 21:09:58 +0300

    progress

--- reducer/src/commonMain/kotlin/com/darkos/mvu/validation/ValidationReducer.kt
+++ reducer/src/commonMain/kotlin/com/darkos/mvu/validation/ValidationReducer.kt
@@ -1,59 +1,51 @@
 package com.darkos.mvu.validation
 
-import com.darkos.mvu.validation.model.ValidationState
-import com.darkos.mvu.Reducer
-import com.darkos.mvu.common.map
 import com.darkos.mvu.model.*
 import com.darkos.mvu.validation.model.FieldValidationStatus
+import com.darkos.mvu.validation.model.ValidationState
 import com.darkos.mvu.validation.model.mvu.ValidationEffect
 import com.darkos.mvu.validation.model.mvu.ValidationMessage
 
 class ValidationReducer<T : MVUState> internal constructor(
     private val mapperTo: (T, ValidationState) -> T,
-    private val mapperFrom: (T)->ValidationState,
-    private val errorEffectBuilder: (()->Effect)?,
-    private val onSuccess: ((T)->StateCmdData<T>)?
+    private val mapperFrom: (T) -> ValidationState,
+    private val errorCmdBuilder: ValidationCmdBuilder<T>?,
+    private val successCmdBuilder: ValidationCmdBuilder<T>?
 ) {
 
     fun callUpdate(
         state: T,
         message: Message
     ): StateCmdData<T> {
-        if(message is ValidationMessage.Success){
-            onSuccess?.invoke(state)?.let {
-                return it
-            }
-        }
-
-        return mapperFrom(state).let {
-            update(it, message)
-        }.map {
-            mapperTo(state, it)
-        }
-    }
-
-    private fun update(
-        state: ValidationState,
-        message: Message
-    ): StateCmdData<ValidationState> {
         return when (message) {
             is ValidationMessage.Triggered -> {
                 StateCmdData(
                     state = state,
-                    effect = ValidationEffect.Validate(state.fields.values.toList())
+                    effect = ValidationEffect.Validate(mapperFrom(state).fields.values.toList())
+                )
+            }
+            is ValidationMessage.Success -> {
+                successCmdBuilder?.build(state) ?: StateCmdData(
+                    state = state,
+                    effect = None
                 )
             }
             is ValidationMessage.Error -> {
-                val map = HashMap(state.fields)
-                message.wrongFields.forEach { wrongId ->
-                    map[wrongId]?.let {
-                        map[wrongId] = it.copy(status = FieldValidationStatus.INVALID)
+                val newState = mapperFrom(state).also { vState ->//todo: need test
+                    HashMap(vState.fields).also { map ->
+                        message.wrongFields.forEach { wrongId ->
+                            map[wrongId]?.let {
+                                map[wrongId] = it.copy(status = FieldValidationStatus.INVALID)
+                            }
+                        }
                     }
+                }.let {
+                    mapperTo(state, it)
                 }
 
-                StateCmdData(
-                    state = state.copy(fields = map),
-                    effect = errorEffectBuilder?.invoke() ?: None
+                errorCmdBuilder?.build(newState) ?: StateCmdData(
+                    state = newState,
+                    effect = None
                 )
             }
             else -> throw IllegalArgumentException()
@@ -62,10 +54,11 @@ class ValidationReducer<T : MVUState> internal constructor(
 
     @ValidationDsl
     class Builder<T : MVUState> {
-        private var errorEffectBuilder: (()->Effect)? = null
         private var mapperTo: ((T, ValidationState) -> T)? = null
         private var mapperFrom: ((T) -> ValidationState)? = null
-        private var onSuccess: ((T)->StateCmdData<T>)? = null
+
+        private var errorCmdBuilder: ValidationCmdBuilder<T>? = null
+        private var successCmdBuilder: ValidationCmdBuilder<T>? = null
 
         fun registerMapperTo(block: (T, ValidationState) -> T) {
             mapperTo = block
@@ -75,68 +68,73 @@ class ValidationReducer<T : MVUState> internal constructor(
             mapperFrom = block
         }
 
-        fun errorEffect(block: ()->Effect){
-            errorEffectBuilder = block
+        fun whenError(block: ValidationCmdBuilder<T>.() -> Unit) {
+            errorCmdBuilder = ValidationCmdBuilder<T>().apply(block)
         }
 
-        fun mapErrorState(block: (T)->T){
-
-        }
-
-        fun error(block: StateCmdBuilder<T>.()->Unit){
-
-        }
-
-        fun processSuccess(block: (T) -> StateCmdData<T>){
-            onSuccess = block
+        fun whenSuccess(block: ValidationCmdBuilder<T>.() -> Unit) {
+            successCmdBuilder = ValidationCmdBuilder<T>().apply(block)
         }
 
         fun build() = ValidationReducer(
             mapperTo = mapperTo!!,
             mapperFrom = mapperFrom!!,
-            errorEffectBuilder = errorEffectBuilder,
-            onSuccess = onSuccess
+            errorCmdBuilder = errorCmdBuilder,
+            successCmdBuilder = successCmdBuilder
         )
     }
 }
 
-class StateCmdBuilder<T: MVUState>{
-    fun effect(block: () -> Effect){
+interface CmdBuilder<T : MVUState> {
+    fun effect(block: (T) -> Effect)
+    fun state(block: (T) -> T)
+    fun build(state: T): StateCmdData<T>
+}
 
+class StateCmdBuilder<T : MVUState> : CmdBuilder<T> {
+    private var effectBuilder: ((T) -> Effect)? = null
+    private var stateBuilder: ((T) -> T)? = null
+
+    override fun effect(block: (T) -> Effect) {
+        effectBuilder = block
     }
 
-    fun state(block: (T) -> T){
-
+    override fun state(block: (T) -> T) {
+        stateBuilder = block
     }
 
-    fun build() = StateCmdData<T>()
+    override fun build(state: T) = StateCmdData<T>(
+        state = stateBuilder?.invoke(state) ?: state,
+        effect = effectBuilder?.invoke(state) ?: None
+    )
 }
 
 @ValidationDsl
-fun <T: MVUState>ValidationReducer(block: ValidationReducer.Builder<T>.() -> Unit) =
+class ValidationCmdBuilder<T : MVUState> : CmdBuilder<T> by StateCmdBuilder()
+
+@ValidationDsl
+fun <T : MVUState> ValidationReducer(block: ValidationReducer.Builder<T>.() -> Unit) =
     ValidationReducer.Builder<T>().apply(block).build()
 
-fun check(){
-    class TestState: MVUState()
+fun check() {
+    class TestState : MVUState()
+
     val r = ValidationReducer<TestState> {
-        processSuccess {
-            StateCmdData(
-                state = it,
-                effect = None
-            )
+        registerMapperFrom {
+            ValidationState()
         }
 
-        error {
+        registerMapperTo { testState, validationState ->
+            testState
+        }
+
+        whenError {
             effect { None }
             state { it }
         }
-
-        errorEffect {
-            None
-        }
-
-        mapErrorState {
-            it
+        whenSuccess {
+            effect { None }
+            state { it }
         }
     }
 }
